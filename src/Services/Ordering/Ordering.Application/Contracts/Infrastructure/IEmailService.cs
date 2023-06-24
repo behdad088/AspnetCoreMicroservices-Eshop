d2dcb8dcc4c619233e0645111cc4172c839c64b5
behdad kardgar <behdad.kardgar1@gmail.com>
2023-06-24 12:03:35 +0200

    Add ordering api
    In this Pr we create ordering api project and added clean architecture layers (ordering.domain, ordering.application and ordering.infrastructure)

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Ordering.Application.Models;
+
+namespace Ordering.Application.Contracts.Infrastructure
+{
+    internal interface IEmailService
+    {
+        Task<bool> SendEmailAsync(Email email);
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,8 @@
+﻿using AutoMapper;
+
+namespace Ordering.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+    }
+}
