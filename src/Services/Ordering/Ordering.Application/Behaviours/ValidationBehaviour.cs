755ef2a2dda86139a13d0e0ef05bab17c8ceb818
behdad kardgar <behdad.kardgar1@gmail.com>
2023-06-26 18:03:13 +0200

    Add unhandled exception and validation behaviour
    Adding unhandled exception and validation behaviour.

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using FluentValidation;
+using MediatR;
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
+
+namespace Ordering.Application.Behaviours
+{
+    /// <summary>
+    /// This class will gets all the validatiors and run throw each one. 
+    /// If the validators has any failures it will throw an <see cref="ValidationException"/>.
+    /// If not it will run the request handler. 
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators ?? throw new ArgumentNullException(nameof(validators));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Count != 0)
+                    throw new ValidationException(failures);
+            }
+
+            return await next();
+        }
+    }
+}
