5c71d605e9930a65668a95e2ccea5e6065794814
behdad kardgar <behdad.kardgar1@gmail.com>
2023-07-15 10:17:19 +0200

    add web bff
    add web backend for frontend. I use Yarp reverse proxy to redirect the traffic from gateway to the micro services

---
+++ src/ApiGateways/Web.Bff.Shopping/aggregator/Extensions/Extensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Web.Shopping.HttpAggregator.Extensions
+{
+    internal static class Extensions
+    {
+        public static IServiceCollection AddReverseProxy(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddReverseProxy().LoadFromConfig(configuration.GetRequiredSection("ReverseProxy"));
+
+            return services;
+        }
+    }
+}
---
+++ src/ApiGateways/Web.Bff.Shopping/aggregator/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Web.Shopping.HttpAggregator
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
