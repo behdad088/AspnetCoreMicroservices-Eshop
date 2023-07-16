e8ef88e96e4d7d445d10b0eb2b944afa700fba22
behdad kardgar <behdad.kardgar1@gmail.com>
2023-07-16 13:13:52 +0200

    add health check to discount api

---
+++ src/Services/Discount/Discount.API/Extensions/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using Services.Common;
+
+namespace Discount.API.Extensions
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddHealthChecks(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDefaultHealthChecks(configuration);
+            services.AddHealthChecks()
+                .AddNpgSql(configuration.GetValue<string>("DatabaseSettings:ConnectionString"), name: "postgres", tags: new[] { "ready", "liveness" });
+
+            return services;
+        }
+    }
+}
