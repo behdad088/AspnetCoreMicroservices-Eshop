1cb4061a6cd55acf37b64f9874751d0f76c7e3f0
behdad kardgar <behdad.kardgar1@gmail.com>
2023-07-16 14:15:23 +0200

    add health to api gateway web app

--- src/ApiGateways/Web.Bff.Shopping/aggregator/Program.cs
+++ src/ApiGateways/Web.Bff.Shopping/aggregator/Program.cs
@@ -1,4 +1,6 @@
+using Services.Common;
 using Web.Shopping.HttpAggregator.Extensions;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks(builder.Configuration);
 
 var app = builder.Build();
+app.MapDefaultHealthChecks();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
