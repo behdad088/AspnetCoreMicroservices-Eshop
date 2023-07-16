e35802faa4ebe8f2c932323db2d2eb8ed73984a9
behdad kardgar <behdad.kardgar1@gmail.com>
2023-07-16 13:31:34 +0200

    add health check to ordering api

---
+++ src/Services/Ordering/Ordering.API/Extensions/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using Services.Common;
+
+namespace Ordering.API.Extensions
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddHealthChecks(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDefaultHealthChecks(configuration);
+
+            var hcBuilder = services.AddHealthChecks();
+            var sqlConnectionString = configuration.GetConnectionString("OrderingConnectionString");
+
+            if (!string.IsNullOrEmpty(sqlConnectionString))
+            {
+                hcBuilder
+                    .AddSqlServer(_ => sqlConnectionString, name: "sql-CatalogDB-check", tags: new string[] { "ready" });
+            }
+
+            return services;
+        }
+    }
+}
