325b89513c1d7e062bcea30230fcb3f55553e732
behdad kardgar <behdad.kardgar1@gmail.com>
2023-08-02 10:08:14 +0200

    add pages

---
+++ src/WebApps/WebMVC/Pages/Cart.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMVC.Models;
+using WebMVC.Services;
+
+namespace WebMVC
+{
+    public class CartModel : PageModel
+    {
+        private readonly IBasketService _basketService;
+
+        public CartModel(IBasketService basketService)
+        {
+            _basketService = basketService ?? throw new ArgumentNullException(nameof(basketService));
+        }
+
+        public BasketModel Cart { get; set; } = new BasketModel();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var userName = "swn";
+            Cart = await _basketService.GetBasket(userName);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostRemoveToCartAsync(string productId)
+        {
+            var userName = "swn";
+            var basket = await _basketService.GetBasket(userName);
+
+            var item = basket.Items.Single(x => x.ProductId == productId);
+            basket.Items.Remove(item);
+
+            var basketUpdated = await _basketService.UpdateBasket(basket);
+
+            return RedirectToPage();
+        }
+    }
+}
---
+++ src/WebApps/WebMVC/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebMVC
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
