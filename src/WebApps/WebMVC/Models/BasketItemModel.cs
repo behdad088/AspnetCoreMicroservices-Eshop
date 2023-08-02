21ac3cd1c852dec647f248cd6d09ef36b3defe66
behdad kardgar <behdad.kardgar1@gmail.com>
2023-08-02 10:06:18 +0200

    adding models

---
+++ src/WebApps/WebMVC/Models/BasketItemModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WebMVC.Models
+{
+    public class BasketItemModel
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
---
+++ src/WebApps/WebMVC/Models/BasketModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WebMVC.Models
+{
+    public class BasketModel
+    {
+        public string UserName { get; set; }
+        public List<BasketItemModel> Items { get; set; } = new List<BasketItemModel>();
+        public decimal TotalPrice { get; set; }
+    }
+}
