589976479b4dc5e4d5b839cbd3e0ade73abd1d48
behdad kardgar <behdad.kardgar1@gmail.com>
2023-08-04 12:03:47 +0200

    add web status

---
+++ src/WebApps/WebStatus/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebStatus.Models;
+
+namespace WebStatus.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var basePath = _configuration["PATH_BASE"];
+            return Redirect($"{basePath}/hc-ui");
+        }
+
+        [HttpGet("/Config")]
+        public IActionResult Config()
+        {
+            var configurationValues = _configuration.GetSection("HealthChecksUI:HealthChecks")
+                .Get<Config[]>().ToDictionary(x => x.Name, x => x.Uri);
+
+            return View(configurationValues);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
