31c6ea7fb1096181d1c3f560dab80d180e330979
behdad kardgar <behdad.kardgar1@gmail.com>
2023-10-07 16:05:29 +0200

    add elastic search logging to WebMVC

--- src/WebApps/WebMVC/Services/OrderService.cs
+++ src/WebApps/WebMVC/Services/OrderService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -10,15 +11,18 @@ namespace WebMVC.Services
     public class OrderService : IOrderService
     {
         private readonly HttpClient _client;
+        private readonly ILogger<OrderService> _logger;
 
-        public OrderService(HttpClient client)
+        public OrderService(HttpClient client, ILogger<OrderService> logger)
         {
             _client = client ?? throw new ArgumentNullException(nameof(client));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
-        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string userName)
+        public async Task<IEnumerable<OrderResponseModel>> GetOrdersByUserName(string username)
         {
-            var response = await _client.GetAsync($"/ordering-api/api/v1/Order/{userName}");
+            _logger.LogInformation("Getting order by username {Username}", username);
+            var response = await _client.GetAsync($"/ordering-api/api/v1/Order/{username}");
             return await response.ReadContentAs<List<OrderResponseModel>>();
         }
     }
