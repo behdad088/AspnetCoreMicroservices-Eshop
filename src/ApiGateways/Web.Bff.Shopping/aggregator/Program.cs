c48188cf89e1494d3365b1e77ea5ce6da9b89db2
Behdad Kardgar <behdad.kardgar1@gmail.com>
2024-03-01 19:47:05 +0100

    add opt to all the services

--- src/ApiGateways/Web.Bff.Shopping/aggregator/Program.cs
+++ src/ApiGateways/Web.Bff.Shopping/aggregator/Program.cs
@@ -1,9 +1,57 @@
 using Eshop.BuildingBlocks.Logging;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Services.Common;
+using System.Diagnostics;
 using Web.Shopping.HttpAggregator.Extensions;
 using Web.Shopping.HttpAggregator.Models.Config;
 
 var builder = WebApplication.CreateBuilder(args);
+const string serviceName = "eshop.shopping.httpaggregator.api";
+var telemetry = new ActivitySource(serviceName);
+builder.Services.AddSingleton(telemetry);
+var resourceBuilder = ResourceBuilder.CreateDefault().AddService(serviceName);
+
+builder.Logging
+    .AddOpenTelemetry(options =>
+    {
+        options.IncludeFormattedMessage = true;
+        options.IncludeScopes = true;
+
+        options.SetResourceBuilder(resourceBuilder);
+        options.AddOtlpExporter();
+    });
+
+builder.Services.Configure<AspNetCoreTraceInstrumentationOptions>(options =>
+{
+    // Filter out instrumentation of the Prometheus scraping endpoint.
+    options.Filter = ctx => ctx.Request.Path != "/metrics";
+});
+
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(b =>
+    {
+        b.AddService(serviceName);
+    })
+    .WithTracing(b => b
+        .SetResourceBuilder(resourceBuilder)
+        .AddAspNetCoreInstrumentation(options => options.RecordException = true)
+        .AddHttpClientInstrumentation(options => options.RecordException = true)
+        .AddEntityFrameworkCoreInstrumentation()
+        .AddSource(telemetry.Name)
+        .AddOtlpExporter())
+    .WithMetrics(b => b
+        .SetResourceBuilder(resourceBuilder)
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddRuntimeInstrumentation()
+        .AddProcessInstrumentation()
+        .AddPrometheusExporter());
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Add services to the container.
 builder.Services.AddReverseProxy(builder.Configuration);
@@ -29,6 +77,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 app.UseAuthorization();
 
 app.MapControllers();
--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -2,9 +2,57 @@ using Discount.API.Extensions;
 using Discount.API.Grpc;
 using Discount.API.Repositories;
 using Eshop.BuildingBlocks.Logging;
+using OpenTelemetry.Instrumentation.AspNetCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Services.Common;
+using System.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
+const string serviceName = "eshop.discount.api";
+var telemetry = new ActivitySource(serviceName);
+builder.Services.AddSingleton(telemetry);
+var resourceBuilder = ResourceBuilder.CreateDefault().AddService(serviceName);
+
+builder.Logging
+    .AddOpenTelemetry(options =>
+    {
+        options.IncludeFormattedMessage = true;
+        options.IncludeScopes = true;
+
+        options.SetResourceBuilder(resourceBuilder);
+        options.AddOtlpExporter();
+    });
+
+builder.Services.Configure<AspNetCoreTraceInstrumentationOptions>(options =>
+{
+    // Filter out instrumentation of the Prometheus scraping endpoint.
+    options.Filter = ctx => ctx.Request.Path != "/metrics";
+});
+
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(b =>
+    {
+        b.AddService(serviceName);
+    })
+    .WithTracing(b => b
+        .SetResourceBuilder(resourceBuilder)
+        .AddAspNetCoreInstrumentation(options => options.RecordException = true)
+        .AddHttpClientInstrumentation(options => options.RecordException = true)
+        .AddEntityFrameworkCoreInstrumentation()
+        .AddSource(telemetry.Name)
+        .AddOtlpExporter())
+    .WithMetrics(b => b
+        .SetResourceBuilder(resourceBuilder)
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddRuntimeInstrumentation()
+        .AddProcessInstrumentation()
+        .AddPrometheusExporter());
+
 builder.Services.MigrateDatabase<Program>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
@@ -36,6 +84,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 app.UseAuthorization();
 
